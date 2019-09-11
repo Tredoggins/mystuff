4a5112d584e881c038527dd7727e3a190c9acc9e
Tredoggins <54610941+Tredoggins@users.noreply.github.com>
2019-09-10 19:34:11 -0500

    Add files via upload

--- waroftheworlds/src/us/mattgreen/Main.java
+++ waroftheworlds/src/us/mattgreen/Main.java
@@ -1,12 +1,17 @@
 package us.mattgreen;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
 
     private final static FileInput indata = new FileInput("the_book.csv");
     private final static Map<String, Integer> map = new HashMap<String, Integer>();
+    private static List<String> myList=new ArrayList<String>();
+    private static Integer currentMost=0;
+    private static Integer lastMost=0;
 
     
     
@@ -44,10 +49,55 @@ public class Main {
 
             }
 
-            // Loop over entries in the map, getting each key/value pair
+
+        }
+        // Loop over entries in the map, getting each key/value pair
+        for(int i=0;i<3;i++) {
             for (Map.Entry<String, Integer> entry : map.entrySet()) {
-                System.out.println(entry.getKey() + " " + entry.getValue());
+                //System.out.println(entry.getKey() + " " + entry.getValue());
+                if(i==0) {
+                    if (entry.getValue().equals(currentMost)) {
+                        myList.add(entry.getKey());
+                    } else if (entry.getValue().compareTo(currentMost)>0) {
+                        myList.clear();
+                        myList.add(entry.getKey());
+                        currentMost = entry.getValue();
+                    }
+                }
+                else{
+                    if (entry.getValue().equals(currentMost)) {
+                        myList.add(entry.getKey());
+                    } else if (entry.getValue().compareTo(currentMost)>0 && entry.getValue().compareTo(lastMost)<0) {
+                        myList.clear();
+                        myList.add(entry.getKey());
+                        currentMost = entry.getValue();
+                    }
+                }
             }
+            switch (i) {
+                case 0:
+                    System.out.println("\nWord(s) that Appear the Most at " + currentMost + " times:\n");
+                    break;
+                case 1:
+                    System.out.println("\nWord(s) that Appear the Second Most at " + currentMost + " times:\n");
+                    break;
+                case 2:
+                    System.out.println("\nWord(s) that Appear the Third Most at " + currentMost + " times:\n");
+                    break;
+            }
+            for(String s:myList){
+                System.out.println(s);
+            }
+            lastMost=currentMost;
+            currentMost=0;
+            myList.clear();
+        }
+        System.out.println("\nWords that appear only once:\n");
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if(entry.getValue().equals(1)) {
+                System.out.println(entry.getKey());
+            }
+
         }
     }
     
